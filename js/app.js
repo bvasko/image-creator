7b841ac3634b587b2b84349c114e3045ca34fb1b
MaxStump13 <stumpmaximilian@gmail.com>
2021-12-21 16:46:45 -0500

    created functions for form submit, api call, and stickerDisplay

--- js/app.js
+++ js/app.js
@@ -1,21 +1,58 @@
-var giphyKey = "bAqrGC0EFBsitN09IxRQsJdQPme35o1E";
-var giphySearchEl = document.querySelector(".giphyForm");
-var giphySearchInputEl = document.querySelector("#giphyInput");
-var giphySearchResultsContainerEl = document.querySelector(".giphyResultsContainer");
-var giphySearchResultsEl = document.querySelector(".giphyResults");
+let giphyKey = "bAqrGC0EFBsitN09IxRQsJdQPme35o1E";
+let giphySearchEl = $(".giphyForm");
+let giphySearchInputEl = $("#giphyInput");
+let giphySearchResultsContainerEl = $(".giphyResultsContainer");
+let giphySearchResultsEl = $(".giphyResults");
+let giphySearchTermEl = $("#giphySearch");
 
-function handleGiphySearch(event){
-    event.preventDefault();
-    var stickerSearch = giphySearchInput.ariaValueMax.trim();
-    if(!stickerSearch){
-        console.error("Need search input");
+function handleGiphySearch(event) {
+  event.preventDefault();
+  let stickerSearch = giphySearchInputEl.val().trim();
+  if (!stickerSearch) {
+    console.error("Need search input");
+  }
+  giphySearchTermEl.text("");
+  giphySearchInputEl.val("");
+  giphySearchTermEl.text(stickerSearch);
+  giphyStickerSearch(stickerSearch);
+}
+
+function giphyStickerSearch(search) {
+  let giphyAPIUrl =
+    "https://api.giphy.com/v1/stickers/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" +
+    search +
+    "&limit=10&rating=g&lang=en";
+
+  fetch(giphyAPIUrl)
+    .then(function (response) {
+      console.log(response);
+      if (!response.ok) {
+        throw response.json();
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getStickers(data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
+
+function getStickers(stickers){
+    let sticker = stickers.data;
+    console.log(sticker[0].title);
+    for(i=0; i<sticker.length;i++){
+        let imageUrl = sticker[i].images.fixed_height_small.url;
+        let imageAlt = sticker[i].title;
+        let imageEl = $("<img>").attr("src",imageUrl);
+
+        giphySearchResultsEl.append(imageEl);
     }
-    giphySearchInput.value = "";
-    // giphySearchResults.value = "";
-    giphyStickerSearch(stickerSearch);
 }
 
-function giphyStickerSearch(search){
 
-}
-giphySearchEl.addEventListener("submit", handleGiphySearch);
+
+giphySearchEl.on("submit", handleGiphySearch);
