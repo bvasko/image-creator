9174771308be9918c6a49b2864d2da98a071c271
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-21 16:54:22 -0500

    added html for img search

---
+++ js/app.js
@@ -0,0 +1,4 @@
+//unsplashed access key = EHxuxyvymZS2_p4tpYQf51gNAOtih5AJF9xo-7UAmzI
+//unsplashed secret key =  fiUIPd3h7dVWI683NCuKsFuj4YAipHr-XnBEr1_s9Ps
+
+//https://api.unsplash.com/photos/?client_id=YOUR_APPLICATION_ID
