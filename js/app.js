0d33b907bc7cddbb31af7991cc89216e0a9cb944
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-23 12:43:48 -0500

    test

--- js/app.js
+++ js/app.js
@@ -9,43 +9,48 @@ var imgSearchResultsContainer = document.querySelector(".imgSearchResultsContain
 var imgSearchResultsEl = document.querySelector("#image");
 
 var formSubmitHandler = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var imgSearchVal = imgSearchInputEl.value.trim();
-    console.log(imgSearchVal);
-    if (imgSearchVal) {
-        getApi(imgSearchVal);
-        // getForecast(imgVal);
-        imgSearchInputEl.value = '';
-    } else {
-        alert('Please enter a City');
-    }
+  var imgSearchVal = imgSearchInputEl.value.trim();
+  console.log(imgSearchVal);
+  if (imgSearchVal) {
+    getApi(imgSearchVal);
+    imgSearchInputEl.value = '';
+  } else {
+    console.error('Please enter a search term!');
+  }
 
 };
 
 function getApi(imgSearchVal) {
-    var accessKey = "EHxuxyvymZS2_p4tpYQf51gNAOtih5AJF9xo-7UAmzI";
-    // var requestUrl = 'https://api.unsplash.com/search/photos?&query=' + imgSearchVal + "/?client_id=" + accessKey;
-    var requestUrl = "https://api.unsplash.com/search/photos?query=" + imgSearchVal + "&content_filter=high&client_id=" + accessKey + "&fm=jpg"
+  var accessKey = "EHxuxyvymZS2_p4tpYQf51gNAOtih5AJF9xo-7UAmzI";
+  // var requestUrl = 'https://api.unsplash.com/search/photos?&query=' + imgSearchVal + "/?client_id=" + accessKey;
+  var requestUrl = "https://api.unsplash.com/search/photos?query=" + imgSearchVal + "&content_filter=high&client_id=" + accessKey + "&fm=jpg"
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
 
-            for (var i = 0; i < 9; i++) {
-                // let imageElement = document.createElement('img');
-                const img = document.createElement("img");
-                img.src = data.results[i].urls.thumb;
-                imgSearchResultsEl.append(img);
-                
-            }
+      for (var i = 0; i < 9; i++) {
+        // let imageElement = document.createElement('img');
+        const img = document.createElement("img");
+        img.src = data.results[i].urls.thumb;
+        img.style.padding = "5px";
+        img.style.border = "3px solid #ffffff";
+        img.style.borderRadius = "10px;"
+        imgSearchResultsEl.append(img);
 
-        });
+      }
+
+    });
 }
 imgSearchEl.addEventListener('submit', formSubmitHandler);
+
+//////////////////////////////////////////////////////////////////////////////
+
 let giphyKey = "bAqrGC0EFBsitN09IxRQsJdQPme35o1E";
 let tenorKey = "6FGOA1MVEPK6";
 let giphySearchEl = $(".giphyForm");
@@ -67,22 +72,22 @@ function handleGiphySearch(event) {
   giphySearchResultsEl.empty();
   giphySearchTermEl.text("");
   giphySearchInputEl.val("");
-//   giphySearchTermEl.text(stickerSearch);
+  //   giphySearchTermEl.text(stickerSearch);
   giphyStickerSearch(stickerSearch);
 }
 
 function giphyStickerSearch(search) {
-    console.log(search);
-    let searchVal = search.replace(/\+/g," ");
-    console.log(searchVal);
-    giphySearchTermEl.text(searchVal);
+  console.log(search);
+  let searchVal = search.replace(/\+/g, " ");
+  console.log(searchVal);
+  giphySearchTermEl.text(searchVal);
 
-    let giphyAPIUrl = "https://api.giphy.com/v1/gifs/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q="+search+"&limit=5&offset=0&rating=g&lang=en&bundlde=fixed_width_small";
-    // let giphyAPIUrl = "https://g.tenor.com/v1/search?q=" + search+ "&" + tenorKey+  "&limit=5&contentfitler=high&media_filter=minimal";
+  let giphyAPIUrl = "https://api.giphy.com/v1/gifs/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" + search + "&limit=5&offset=0&rating=g&lang=en&bundlde=fixed_width_small";
+  // let giphyAPIUrl = "https://g.tenor.com/v1/search?q=" + search+ "&" + tenorKey+  "&limit=5&contentfitler=high&media_filter=minimal";
 
-    // "https://api.giphy.com/v1/stickers/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" +
-    // search +
-    // "&offset=0&rating=g&lang=en";
+  // "https://api.giphy.com/v1/stickers/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" +
+  // search +
+  // "&offset=0&rating=g&lang=en";
 
   fetch(giphyAPIUrl)
     .then(function (response) {
@@ -94,39 +99,40 @@ function giphyStickerSearch(search) {
     })
     .then(function (data) {
       console.log(data);
-      if(data.data.length == 0){
+      if (data.data.length == 0) {
         giphySearchTermEl.text("No results found for: " + search);
         console.log(data);
-        }
-        else{
+      }
+      else {
         getStickers(data);
-    }})
+      }
+    })
     .catch(function (error) {
       console.error(error);
     });
 }
 
 
-function getStickers(stickers){
-    let sticker = stickers.data;
-    console.log(sticker[0].title);
-    for(i=0; i<sticker.length;i++){
-      let imageUrl = sticker[i].images.fixed_height_small.url;
-      let imageAlt = sticker[i].title;
-      let imageEl = $("<img>").attr({"src": imageUrl, "alt" : imageAlt, "title" :imageAlt});
-      imageEl.on("click", pasteSticker);
-      giphySearchResultsEl.append(imageEl);
-    }
+function getStickers(stickers) {
+  let sticker = stickers.data;
+  console.log(sticker[0].title);
+  for (i = 0; i < sticker.length; i++) {
+    let imageUrl = sticker[i].images.fixed_height_small.url;
+    let imageAlt = sticker[i].title;
+    let imageEl = $("<img>").attr({ "src": imageUrl, "alt": imageAlt, "title": imageAlt });
+    imageEl.on("click", pasteSticker);
+    giphySearchResultsEl.append(imageEl);
+  }
 }
 
-function pasteSticker(event){
-    event.preventDefault();
-    console.log($(this));
-    let customizableSticker = $(this);
-    customizableSticker.clone().appendTo(imgContainerEl);
+function pasteSticker(event) {
+  event.preventDefault();
+  console.log($(this));
+  let customizableSticker = $(this);
+  customizableSticker.clone().appendTo(imgContainerEl);
 }
 
-function positionSticker(){
+function positionSticker() {
 
 }
 
