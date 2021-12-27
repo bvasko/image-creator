5fcf0200782d529209ba0ef3c75966faea0d7e17
Warren Haskins <warrenhaskins1@gmail.com>
2021-12-27 13:20:10 -0500

    added modal for alert, finished img-search

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var imgSearchEl = document.querySelector("#imgForm");
 var imgSearchInputEl = document.querySelector("#img-search");
 
-var imgSearchResultsContainer = document.querySelector(".imgSearchResultsContainer");
+var imgSearchResultsContainer = document.querySelector(
+  ".imgSearchResultsContainer"
+);
 var imgSearchResultsEl = document.querySelector("#image");
 
 var formSubmitHandler = function (event) {
@@ -11,24 +13,28 @@ var formSubmitHandler = function (event) {
   console.log(imgSearchVal);
   if (imgSearchVal) {
     getApi(imgSearchVal);
-    imgSearchInputEl.value = '';
+    imgSearchInputEl.value = "";
   } else {
-    //TODO: change this to a modal
-    alert('Please enter a search term');
+    $("#imgSearch-modal").modal({});
+    $("#imgSearch-modal").modal("open");
   }
-
 };
 
 function getApi(imgSearchVal) {
   var accessKey = "EHxuxyvymZS2_p4tpYQf51gNAOtih5AJF9xo-7UAmzI";
-  var requestUrl = "https://api.unsplash.com/search/photos?query=" + imgSearchVal + "&orientation=squarish&content_filter=high&client_id=" + accessKey + "&fm=jpg"
+  var requestUrl =
+    "https://api.unsplash.com/search/photos?query=" +
+    imgSearchVal +
+    "&orientation=squarish&content_filter=high&client_id=" +
+    accessKey +
+    "&fm=jpg";
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      console.log(data);
 
       for (let i = 0; i < 9; i++) {
         const img = document.createElement("img");
@@ -43,23 +49,18 @@ function getApi(imgSearchVal) {
           console.log(image);
           image.src = selectedImg;
           // document.querySelector(".module-inside").style. backgroundImage = "url('"+selectedImg+"')";
-          imgDisplay = document.querySelector("#image-container");
-          imgDisplay.style.setProperty(`--background-image-url`, selectedImg);
+          imgDisplay = document.querySelector("#image-container").style;
+          imgDisplay.setProperty(
+            "--background-image-url",
+            "url('" + selectedImg + "')"
+          );
           console.log(imgDisplay);
-          
-        })
+        });
         imgSearchResultsEl.append(img);
       }
-
-
-
-
     });
-    
 }
-imgSearchEl.addEventListener('submit', formSubmitHandler);
-
-
+imgSearchEl.addEventListener("submit", formSubmitHandler);
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -72,7 +73,6 @@ let giphySearchResultsEl = $(".giphyResults");
 let giphySearchTermEl = $("#giphySearch");
 let imgContainerEl = $("#image-container .module-inside");
 let filterIcons = $(".card-image");
-
 
 function handleGiphySearch(event) {
   event.preventDefault();
@@ -94,7 +94,10 @@ function giphyStickerSearch(search) {
   console.log(searchVal);
   giphySearchTermEl.text(searchVal);
 
-  let giphyAPIUrl = "https://api.giphy.com/v1/gifs/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" + search + "&limit=5&offset=0&rating=g&lang=en&bundlde=fixed_width_small";
+  let giphyAPIUrl =
+    "https://api.giphy.com/v1/gifs/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" +
+    search +
+    "&limit=5&offset=0&rating=g&lang=en&bundlde=fixed_width_small";
   // let giphyAPIUrl = "https://g.tenor.com/v1/search?q=" + search+ "&" + tenorKey+  "&limit=5&contentfitler=high&media_filter=minimal";
 
   // "https://api.giphy.com/v1/stickers/search?api_key=bAqrGC0EFBsitN09IxRQsJdQPme35o1E&q=" +
@@ -114,8 +117,7 @@ function giphyStickerSearch(search) {
       if (data.data.length == 0) {
         giphySearchTermEl.text("No results found for: " + search);
         console.log(data);
-      }
-      else {
+      } else {
         getStickers(data);
       }
     })
@@ -124,14 +126,17 @@ function giphyStickerSearch(search) {
     });
 }
 
-
 function getStickers(stickers) {
   let sticker = stickers.data;
   console.log(sticker[0].title);
   for (i = 0; i < sticker.length; i++) {
     let imageUrl = sticker[i].images.fixed_height_small.url;
     let imageAlt = sticker[i].title;
-    let imageEl = $("<img>").attr({ "src": imageUrl, "alt": imageAlt, "title": imageAlt });
+    let imageEl = $("<img>").attr({
+      src: imageUrl,
+      alt: imageAlt,
+      title: imageAlt,
+    });
     imageEl.on("click", pasteSticker);
     giphySearchResultsEl.append(imageEl);
   }
@@ -144,15 +149,13 @@ function pasteSticker(event) {
   customizableSticker.clone().appendTo(imgContainerEl);
 }
 
-function positionSticker() {
-
-}
+function positionSticker() {}
 
 function applyFilter(event) {
   event.stopPropagation();
   event.stopImmediatePropagation();
   const filterType = event.currentTarget.firstElementChild.dataset.filter;
-  display = document.querySelector('#image-container');
+  display = document.querySelector("#image-container");
   display.style.setProperty(`--filter-type`, `${filterType}`);
 }
 
