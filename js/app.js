1a540021ef3911fe9fc93ec34207fd39262b5278
Bonnie DiPasquale <bonnie.the.builder@icloud.com>
2021-12-31 11:14:13 -0500

    make filter slides work

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, options);
+  // var instances = M.Sidenav.init(elems, options);
 });
 $(document).ready(function(){
   $('.sidenav').sidenav();
